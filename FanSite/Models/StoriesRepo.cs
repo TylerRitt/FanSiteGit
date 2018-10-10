5de79a01bc75f023a038877da0127affb5dd6291
unknown <tylerritter3636@gmail.com>
2018-10-09 20:13:01 -0700

    added tables and classes still buggy beta

---
+++ FanSite/Models/StoriesRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanSite.Models
+{
+    public static class StoriesRepo
+    {
+        private static List<Stories> stories = new List<Stories>();
+
+        public static List<Stories> Stories { get { return stories; } }
+        
+        public static void AddStory(Stories story)
+        {
+            stories.Add(story);
+        }
+
+    }
+}
